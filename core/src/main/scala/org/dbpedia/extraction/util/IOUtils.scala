e57959a853ac86fdc8426440beaef422b4bed6ef
praveen <praveen@blippar.com>
2016-05-26 16:01:46 -0700

    - revert buffered

--- core/src/main/scala/org/dbpedia/extraction/util/IOUtils.scala
+++ core/src/main/scala/org/dbpedia/extraction/util/IOUtils.scala
@@ -2,7 +2,7 @@ package org.dbpedia.extraction.util
 
 import org.apache.commons.compress.compressors.bzip2.{BZip2CompressorInputStream,BZip2CompressorOutputStream}
 import java.util.zip.{GZIPInputStream,GZIPOutputStream}
-import java.io.{InputStream,OutputStream,Reader,Writer,OutputStreamWriter,InputStreamReader,BufferedOutputStream,BufferedInputStream}
+import java.io.{InputStream,OutputStream,Reader,Writer,OutputStreamWriter,InputStreamReader}
 import scala.io.Codec
 import org.dbpedia.extraction.util.RichReader.wrapReader
 import java.nio.charset.Charset
@@ -56,14 +56,14 @@ object IOUtils {
    * wrap in writer.
    */
   def writer(file: FileLike[_], charset: Charset = Codec.UTF8.charSet): Writer =
-    new OutputStreamWriter(new BufferedOutputStream(outputStream(file), 20 * 1024 * 1024), charset)
+    new OutputStreamWriter(outputStream(file), charset)
   
   /**
    * open input stream, wrap in unzipper stream if file suffix indicates compressed file,
    * wrap in reader.
    */
   def reader(file: FileLike[_], charset: Charset = Codec.UTF8.charSet): Reader =
-    new InputStreamReader(new BufferedInputStream(inputStream(file), 20 * 1024 * 1024), charset)
+    new InputStreamReader(inputStream(file), charset)
   
   /**
    * open input stream, wrap in unzipper stream if file suffix indicates compressed file,
